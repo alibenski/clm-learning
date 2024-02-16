fe12ec8fa8a4e9eba81844f85dd90cb91483bf43
Allyson S. Frias <allyson.frias@gmail.com>
2024-02-16 21:37:31 +0100

    install cofee Z theme

---
+++ web/themes/custom/clm_theme/js/clm-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * clm_theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.clmTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
