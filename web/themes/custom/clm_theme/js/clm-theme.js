0dc85d2c769155c624c9521975d9cbe6b1f56e8e
Allyson S. Frias <allyson.frias@gmail.com>
2024-02-21 12:05:11 +0100

    bootstrap_barrio theme and create subtheme clm_barrio

--- web/themes/custom/clm_theme/js/clm-theme.js
+++ web/themes/custom/clm_theme/js/clm-theme.js
@@ -1,17 +1,45 @@
-/**
- * @file
- * clm_theme behaviors.
- */
-(function (Drupal) {
+/* --------------------------------------------- 
+* Filename:     custom.js
+* Version:      1.0.0 (Tue 26 Mar 2019)
+				1.0.2 (2024-01-09)
+* Website:      https://www.zymphonies.com
+* Description:  Global Script
+* Author:       Zymphonies Team
+                info@zymphonies.com
+-----------------------------------------------*/
 
-  'use strict';
+function theme_menu(){
 
-  Drupal.behaviors.clmTheme = {
-    attach (context, settings) {
+	//Main menu
+	jQuery('#main-menu').smartmenus();
+	
+	//Mobile menu toggle
+	jQuery('.navbar-toggle').click(function(){
+		jQuery('.region-primary-menu').addClass('expand');
+	});
+	jQuery('.navbar-toggle-close').click(function(){
+		jQuery('.region-primary-menu').removeClass('expand');
+	});
 
-      console.log('It works!');
+	//Mobile drop-down menu
+	if ( jQuery(window).width() < 767) {
+		jQuery(".region-primary-menu li a:not(.has-submenu)").click(function () {
+			jQuery('.region-primary-menu').hide();
+	    });
+	}
 
-    }
-  };
+}
 
-} (Drupal));
+function theme_home(){
+	
+	//flexslider
+	jQuery('.flexslider').flexslider({
+    	animation: "slide"	
+    });
+
+}
+
+jQuery(document).ready(function($){
+	theme_menu();
+	theme_home();
+});
